fc141e90a6d1bc5ab6643b446fd339fc677b6292
Monali <MP@mp.com>
2025-03-08 20:23:11 +0530

    [ADD] | Added landing page 4 & 5

--- kreator_website/controllers/main.py
+++ kreator_website/controllers/main.py
@@ -7,13 +7,21 @@ from odoo.http import request
 class KreatorWebsite(http.Controller):
 
     @http.route('/landing/page/1', auth='public', website=True)
-    def landing_page_1(self, **kw):
+    def landing_page_1(self):
         return request.render("kreator_website.landing_page_1", {})
 
     @http.route('/landing/page/2', auth='public', website=True)
-    def landing_page_2(self, **kw):
+    def landing_page_2(self):
         return request.render("kreator_website.landing_page_2", {})
 
     @http.route('/landing/page/3', auth='public', website=True)
-    def landing_page_3(self, **kw):
+    def landing_page_3(self):
         return request.render("kreator_website.landing_page_3", {})
+
+    @http.route('/landing/page/4', auth='public', website=True)
+    def landing_page_4(self):
+        return request.render("kreator_website.landing_page_4", {})
+
+    @http.route('/landing/page/5', auth='public', website=True)
+    def landing_page_5(self):
+        return request.render("kreator_website.landing_page_5", {})
