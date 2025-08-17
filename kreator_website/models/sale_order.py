4a73d314525bae6d4d2ad525ab179926abfb4b8c
Himanshu <hkapri.hk@gmail.com>
2025-08-17 15:22:23 +0530

    fix multi so creation

--- kreator_website/models/sale_order.py
+++ kreator_website/models/sale_order.py
@@ -4,11 +4,13 @@ from odoo import models, fields, api, _
 class SalesOrder(models.Model):
     _inherit = 'sale.order'
 
-    def generate_sale_for_users(self, product_ids=[]):
-        products = self.env['product.template'].search([('id', 'in', product_ids)]).product_variant_ids
-        order_line = [(0, 0, {'product_id': product.id, 'product_uom_qty': 1}) for product in products]
-        so_partners = self.env['sale.order'].sudo().search([]).partner_id.ids
-        for contact in self.env['res.partner'].sudo().search([('user_type', '=', 'partner'), ('id', 'not in', so_partners)]):
+    def generate_sale_for_users(self, partner_products=[]):
+        subscription = self.env['product.template'].search([('id', '=', 107)]).product_variant_ids
+        for partner, products in partner_products:
+            contact = self.env['res.partner'].sudo().search([('id', '=', partner)])
+            product_variants = self.env['product.template'].sudo().search([('id', 'in', products)]).product_variant_ids
+            order_line = [(0, 0, {'product_id': product.id, 'product_uom_qty': 1}) for product in product_variants]
+            order_line.append((0, 0, {'product_id': subscription.id, 'product_uom_qty': 1}))
             order = self.create({
                 'partner_id': contact.id,
                 'partner_invoice_id': contact.id,
